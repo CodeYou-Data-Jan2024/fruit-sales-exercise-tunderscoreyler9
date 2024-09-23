93a4300d2ef3682c6cdf66060df15d568fc2a844
Tyler <tyler.s9@proton.me>
2024-09-22 23:06:17 -0400

    Submitting fruit_sales Python assignment

--- fruitsales.py
+++ fruitsales.py
@@ -1,1 +1,14 @@
 # add your code here
+import pandas as pd
+
+data = {
+    'Apples': [35, 41],
+    'Bananas': [21, 34],
+}
+
+index = ['2017 Sales', '2018 Sales']
+
+df = pd.DataFrame(data, index=index)
+df.to_csv('fruit.csv')
+
+print("`fruitsales.py` has written data to `fruit.csv`!")
